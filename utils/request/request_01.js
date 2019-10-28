e50bbed6da2cf6e86264a1532072206d05580874
xuwangd <xuwangd@163.com>
2019-10-28 19:01:14 +0800

    xw

--- utils/request/request_01.js
+++ utils/request/request_01.js
@@ -6,9 +6,9 @@ const alert = require('../tool/alert.js');
 
 const tool = require('../tool/tool.js');
 
-const SERVICE = "https://game.flyh5.cn/game/wx7c3ed56f7f792d84/yyt_dfqcfslb/public";
+// const SERVICE = "https://game.flyh5.cn/game/wx7c3ed56f7f792d84/yyt_dfqcfslb/public";
 // const SERVICE = "http://dfldata-test.dongfeng-nissan.com.cn/fslb/public/index.php";
-// const SERVICE = "https://weixinfslb.venucia.com";
+const SERVICE = "https://weixinfslb.venucia.com";
 
 //版本控制
 const tag = (data) => {
