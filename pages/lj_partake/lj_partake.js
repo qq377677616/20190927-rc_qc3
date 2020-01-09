c03436f436b385232563b6b7b7c2ad313cd59e47
勇 <1004511278@qq.com>
2020-01-09 18:37:22 +0800

    zcy

--- pages/lj_partake/lj_partake.js
+++ pages/lj_partake/lj_partake.js
@@ -16,49 +16,49 @@ Page({
    * 页面的初始数据
    */
   data: {
-    IMGSERVICEZ: app.globalData.IMGSERVICE, 
-    image:'',
-    content:'',
-    cursor:0,
+    IMGSERVICEZ: app.globalData.IMGSERVICE,
+    image: '',
+    content: '',
+    cursor: 0,
   },
 
   // 上传图片获取图片地址
   uploadPic() {
     let that = this;
-    
-      wx.chooseImage({
-        success(res) {
-          tool.loading('加载中')
-          const tempFilePaths = res.tempFilePaths
-          tool.getImageInfo(tempFilePaths[0]).then(res => {
-            console.log("res", res)
-            if (res.width < 573 || res.height < 760) {
-              tool.showModal("图片尺寸", "上传图片尺寸不能小于573*760,请重新选择/拍摄照片进行上传", "好的,#1354BB", false)
-              tool.loading_h()
-              return
-            }
-            wx.uploadFile({
-              url: 'https://game.flyh5.cn/game/wx7c3ed56f7f792d84/yyt_dfqcfslb/public/api3/upload/upload_image', 
-              filePath: tempFilePaths[0],
-              name: 'file',
-              success(res) {
-                let data = JSON.parse(res.data);
-                let image = data.data.src;
-                that.setData({
-                  image,
-                })
-                tool.loading_h();
-              },
-            })
+
+    wx.chooseImage({
+      success(res) {
+        tool.loading('加载中')
+        const tempFilePaths = res.tempFilePaths
+        tool.getImageInfo(tempFilePaths[0]).then(res => {
+          console.log("res", res)
+          if (res.width < 573 || res.height < 760) {
+            tool.showModal("图片尺寸", "上传图片尺寸不能小于573*760,请重新选择/拍摄照片进行上传", "好的,#1354BB", false)
+            tool.loading_h()
+            return
+          }
+          wx.uploadFile({
+            url: 'https://game.flyh5.cn/game/wx7c3ed56f7f792d84/yyt_dfqcfslb/public/api3/upload/upload_image',
+            filePath: tempFilePaths[0],
+            name: 'file',
+            success(res) {
+              let data = JSON.parse(res.data);
+              let image = data.data.src;
+              that.setData({
+                image,
+              })
+              tool.loading_h();
+            },
           })
-        } 
-      })
-      tool.loading_h()
+        })
+      }
+    })
+    tool.loading_h()
   },
 
   // 获取textarea文本值
   bindinput(e) {
-    console.log('e',e)
+    console.log('e', e)
     let content = e.detail.value;
     let cursor = e.detail.cursor;
     this.setData({
@@ -68,20 +68,50 @@ Page({
   },
 
   // 点击提交
-  formSubmit(e) {
+  submitInfo() {
+    var _this = this
+    if (wx.getStorageSync('voteInfo') != 1) { //订阅消息暂时不用
+      wx.requestSubscribeMessage({
+        tmplIds: ['I_lrsh0UX78QEn1RuXoT8KPwII01ZkB_i_X98CM6aDc'],
+        success(res) {
+          if (res['I_lrsh0UX78QEn1RuXoT8KPwII01ZkB_i_X98CM6aDc'] == "accept") {
+            wx.setStorageSync('voteInfo', 1)
+            _this.submit()
+          }else{
+            wx.setStorageSync('voteInfo', 0)
+            _this.submit()
+          }
+        },
+        fail(err) {
+          console.log(err.errCode)
+          console.log("不接受", err)
+        }
+      })
+    }
+  },
+
+  // 提交方法
+  submit() {
     const activity_id = this.data.activity_id;
     console.log("activity_id", activity_id)
     const user_id = wx.getStorageSync('userInfo').user_id;
     const openid = wx.getStorageSync('userInfo').openid;
     let content = this.data.content;
     let image = this.data.image;
-    let form_id = e.detail.formId;
-    if(image==""){
+    let form_id = wx.getStorageSync('voteInfo');
+    if (image == "") {
       tool.alert('请上传照片');
-    } else if (content==""){
+    } else if (content == "") {
       tool.alert('请填写内容');
-    }else{
-      request_05.uploadVote({ user_id, openid, activity_id, content, image, form_id }).then(res => {
+    } else {
+      request_05.uploadVote({
+        user_id,
+        openid,
+        activity_id,
+        content,
+        image,
+        form_id
+      }).then(res => {
         console.log(res);
         let status = res.data.status;
         console.log(status);
@@ -106,15 +136,14 @@ Page({
         }
       })
     }
-
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
     this.setData({
-      activity_id:options.activity_id
+      activity_id: options.activity_id
     })
     console.log(options.activity_id)
   },
@@ -122,49 +151,49 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
+  onReady: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
+  onShow: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
+  onHide: function() {
 
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
+  onUnload: function() {
 
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
+  onPullDownRefresh: function() {
 
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom: function () {
+  onReachBottom: function() {
 
   },
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage: function () {
+  onShareAppMessage: function() {
 
   }
 })
--- pages/shake_shake/shake_shake.js
+++ pages/shake_shake/shake_shake.js
@@ -38,6 +38,9 @@ Page({
      * 生命周期函数--监听页面加载
      */
     onLoad: function(options) {
+      tool.getSystemInfo().then(res => {
+        console.log("res", res)
+      })
         request_01.login(() => {
             this.initData(options);
         })
