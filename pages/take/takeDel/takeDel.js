f2db2a8e92319f68780e0ec087f5a3e1961bb11e
songziquan644 <644835157@qq.com>
2020-03-04 18:59:44 +0800

    yls

--- pages/take/takeDel/takeDel.js
+++ pages/take/takeDel/takeDel.js
@@ -79,6 +79,7 @@ Page({
 			socketOpen = true
 			console.log('连接成功！');
 			self.setData({socketOpen:true})
+			self.bindUse();
 			// console.log(res);
 			// for (let i = 0; i < socketMsgQueue.length; i++) {
 			// 	sendSocketMessage(socketMsgQueue[i])
@@ -90,13 +91,26 @@ Page({
 	getval(e){//保存消息
 	  this.setData({msg:e.detail.value});
 	},
+	bindUse(){// 绑定 
+		let time = null;
+		wx.sendSocketMessage({
+			data: '{"type":"bind","uid":"1"}'
+		})
+
+		clearInterval(time);
+		time = setInterval(()=>{ // 发送心跳
+			wx.sendSocketMessage({
+				data: '{"type":"ping"}'
+			})
+		},50000)
+	},
 	sendMsg(){// 发送消息
 		let msg = this.data.msg;
 		msg = msg.replace(/^\s+|\s+$/g, '');
 		if(msg==''){
 			console.log("输入不能为空")
 		}else{
-			let content = JSON.stringify({ "type": "send", "to_uid": "50A", "data": { "msg_type": "1", "content": `${msg}` }}); 
+			let content = `{"type":"send","to_uid":"50A","data":{"msg_type":"1","content":"${msg}"}}`; 
 			if (this.data.socketOpen) {
 				console.log(1)
 				wx.sendSocketMessage({
